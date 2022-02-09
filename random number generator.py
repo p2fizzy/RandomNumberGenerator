86cf1bc7578f2969492209d97d8f761084cd7dec
fizzy <99197411+fizgog68@users.noreply.github.com>
2022-02-09 14:48:34 +0000

    Add files via upload

---
+++ random number generator.py
@@ -0,0 +1,11 @@
+import random
+
+# random integer from 0 to 9
+num1 = random.randint(0, 9)
+print(num1)
+# output 5
+
+# Random integer from 10 to 100
+num2 = random.randint(10, 100)
+print(num2)
+# Output 84
